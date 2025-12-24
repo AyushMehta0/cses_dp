1d1acc50553c74297b794bd28f64cebc890d8b00
Ayush Mehta <aymehta04@gmail.com>
2025-12-24 10:25:42 +0530

    Initial commit

---
+++ dicecombinations.cpp
@@ -0,0 +1,22 @@
+//https://cses.fi/problemset/task/1633
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9+7;
+int main (){
+    int n; cin>>n;
+    vector<int>dp(n+1,0);
+    dp[0] = 1;
+    for (int i = 1;i<=n;i++){
+        for (int j = 1;j<=6;j++){
+            if (i>=j){
+                dp[i]+=dp[i-j];
+                if (dp[i]>MOD)dp[i]-=MOD;
+            }
+        }
+    }
+
+    cout<<dp[n]<<endl;
+    
+}
